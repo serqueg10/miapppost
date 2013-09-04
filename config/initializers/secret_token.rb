117da44cde2c5922b7a68049a8cddc9e5c3ba093
Serqueg10 <sergiogr9506@gmail.com>
2013-09-04 09:11:51 -0500

    otra vez

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Miapppost::Application.config.secret_key_base = 'abe877aa6c287364492b3d8d78f4f446d8c2fc681733fdbbbfb275c413ebfdc64cfa5d166a7bb077d03fe76d79dd6956f4d6d09b6b21390ed0e120d51a6b27c9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Miapppost::Application.config.session_store :cookie_store, key: '_miapppost_session'
